edf35242ab675ee96acedb5eb561264ba34c7388
rgillespie23 <rgillespie@uchicago.edu>
2023-01-27 02:53:04 +0000

    Finished Part 2

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -11,4 +11,40 @@ class MathController < ApplicationController
     
     render({ :template => "math_templates/subtraction_form.html.erb"})
   end
+
+  def add_form
+    render({ :template => "math_templates/addition_form.html.erb"})
+  end
+
+  def add_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @second + @first
+
+    render({ :template => "math_templates/addition_results.html.erb"})
+  end
+
+  def multiply_form
+    render({ :template => "math_templates/multiplication_form.html.erb"})
+  end
+  
+  def multiply_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @second * @first
+
+    render({ :template => "math_templates/multiplication_results.html.erb"})
+  end
+
+  def divide_form
+    render({ :template => "math_templates/division_form.html.erb"})
+  end
+
+  def divide_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first / @second
+    
+    render({ :template => "math_templates/division_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,15 @@ Rails.application.routes.draw do
   get("/subtract", :controller => "math", :action => "minus_form")
 
   get("/add", :controller => "math", :action => "add_form")
+
+  get("/wizard_add", :controller => "math", :action => "add_results")
+
+  get("/multiply", :controller => "math", :action => "multiply_form")
+
+  get("/wizard_multiply", :controller => "math", :action => "multiply_results")
+
+  get("/divide", :controller => "math", :action => "divide_form")
+
+  get("/wizard_divide", :controller => "math", :action => "divide_results")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
